385f7cf66c50fc4786cacf867ecd627769b7dfa7
Abhisek Nayak <cv07abhisek@gmail.com>
2022-10-02 23:01:04 +0530

    BusDriver Location Added

---
+++ app/src/main/java/com/example/mybus/Bus1Maps_Activity.java
@@ -0,0 +1,172 @@
+package com.example.mybus;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.mybus.databinding.Bus1locationMapsBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Bus1Maps_Activity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private Bus1locationMapsBinding binding;
+
+    private DatabaseReference reference;
+    private LocationManager manager;
+
+    private final int MIN_TIME=1000;
+    private final int MIN_DISTANCE = 1;
+
+     Marker myMarker;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = Bus1locationMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        manager= (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        reference=FirebaseDatabase.getInstance().getReference().child("User-101");
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        getLocationUpdates();
+
+        readChanges();
+    }
+
+    private void readChanges() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    try {
+                        MyLocation location = dataSnapshot.getValue(MyLocation.class);
+                        if (location != null) {
+                            myMarker.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+                            LatLng Bus = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Bus, 18), 5000, null);
+
+                        }
+                    }catch (Exception e){
+                        Toast.makeText(Bus1Maps_Activity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getLocationUpdates() {
+        if (manager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+            {
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else {
+                    Toast.makeText(this, "No Provider Enabled", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+          getLocationUpdates();
+        }else {
+            Toast.makeText(this, "Permission Required", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Berhampur and move the camera
+        LatLng Bam = new LatLng(19.314962, 84.794090);
+//        DataSnapshot dataSnapshot1 = null;
+//        MyLocation flocation =dataSnapshot1.getValue(MyLocation.class);
+//        LatLng Bus = new LatLng();
+        myMarker = mMap.addMarker(new MarkerOptions().position(Bam).title("Marker in Bam")
+                .icon(bitmapDescriptorFromVector(getApplicationContext(), R.drawable.ic_baseline_directions_bus_24)));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Bam, 18), 5000, null);
+    }
+
+
+    //For Bus Marker
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawables = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawables.setBounds(0,0, vectorDrawables.getIntrinsicWidth(), vectorDrawables.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawables.getIntrinsicWidth(), vectorDrawables.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawables.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        if (location!=null){
+            saveLocation(location);
+        }else{
+            Toast.makeText(this, "No location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveLocation(Location location) {
+
+        reference.setValue(location);
+    }
+}
--- app/src/main/java/com/example/mybus/MainActivity.java
+++ app/src/main/java/com/example/mybus/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.mybus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private View bus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        FirebaseDatabase.getInstance().getReference().setValue("This is tracker app");
+
+
         setContentView(R.layout.activity_main);
+        bus=findViewById(R.id.bus1);
+        bus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,Bus1Maps_Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
